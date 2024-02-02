fa0636d647cb86919c7bacfab9514dd1fa875dda
spencerstarky <starkyspencer@gmail.com>
2024-02-01 21:57:39 -0500

    Plays 5 rounds of RPS and announces winner. Conditions not returning proper message. If else statements are correct.

--- myscripts.js
+++ myscripts.js
@@ -1,10 +1,7 @@
-console.log('HI')
-
 const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
   const choice = options[Math.floor(Math.random() * options.length)];
-  console.log(choice)
   return choice;
 }
 
@@ -22,20 +19,66 @@ function checkWinner(playerSelection, computerSelection) {
   else {
     return "Computer";
   }
+
 }
+
 function playRound(playerSelection, computerSelection){
-  const result = checkWinner(playerSelection, computerSelection)
+  const result = checkWinner(playerSelection, computerSelection);
   if(result == "Tie"){
     return "It's a tie!"
   }
   else if(result == "Player"){
-    return "Congrats, you are the winner"
+    return "You win"
   }
   else{
     return "You lost."
   }
 }
 
-const playerSelection = "rock";
- const computerSelection = getComputerChoice();
- console.log(playRound(playerSelection, computerSelection));
+function getPlayerChoice(){
+  let validatedInput = false;
+  while(validatedInput == false){
+    const choice = prompt("Rock Paper Scissors");
+    if(choice == null){
+      continue;
+    }
+    const choiceInLower = choice.toLowerCase();
+    if(options.includes(choiceInLower)){
+      validatedInput = true;
+      return choiceInLower;
+    }
+  }
+}
+
+function game(){
+  let scorePlayer = 0;
+  let scoreComputer = 0;
+  console.log("Welcome!")
+  for (let i = 0; i < 5; i ++) {
+   const playerSelection = getPlayerChoice();
+   const computerSelection = getComputerChoice();
+   console.log(playRound(playerSelection,computerSelection));
+   console.log("---------")
+   if(checkWinner(playerSelection,computerSelection) == "Player"){
+    scorePlayer ++;
+   }
+   else if (checkWinner(playerSelection,computerSelection) == "Computer"){
+scoreComputer ++;
+   }
+  }
+  console.log("Game Over")
+  if(scorePlayer > scoreComputer){
+    console.log("Player was the winner")
+  }
+  else if(scorePlayer < scoreComputer){
+    console.log("Computer was the winner");
+  }
+  else{
+    console.log("We have a tie")
+  }
+}
+
+game()
+
+// Conditions aren't matching up as they should.//
+// checkWinner conditions appear to be correct, but the winner, loser, tie isn't popping up when it should.//
